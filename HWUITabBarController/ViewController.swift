21932e66649ca3e55c406ae06c5834961e34055a
Moskalenko Nikita <n.moskalneko1996@yahoo.com>
2020-03-14 21:56:43 +0300

    Initial Commit

---
+++ HWUITabBarController/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HWUITabBarController
+//
+//  Created by Nikita Moskalenko on 3/14/20.
+//  Copyright © 2020 Nikita Moskalenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
